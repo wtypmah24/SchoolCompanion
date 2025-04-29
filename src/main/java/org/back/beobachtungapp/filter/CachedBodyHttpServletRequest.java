4b1c6fd6b9f41a2f82d168d250778badea58e3bf
wtypmah24 <wtypmah24@gmail.com>
2025-04-29 12:53:31 +0200

    fix: lefthook / vulnerabilities / security bugs

--- src/main/java/org/back/beobachtungapp/filter/CachedBodyHttpServletRequest.java
+++ src/main/java/org/back/beobachtungapp/filter/CachedBodyHttpServletRequest.java
@@ -3,6 +3,8 @@ package org.back.beobachtungapp.filter;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,14 +12,23 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
+@Slf4j
 public class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
 
   private final byte[] cachedBody;
 
-  public CachedBodyHttpServletRequest(HttpServletRequest request) throws IOException {
+  public CachedBodyHttpServletRequest(HttpServletRequest request) {
     super(request);
-    InputStream inputStream = request.getInputStream();
-    this.cachedBody = inputStream.readAllBytes();
+    this.cachedBody = readRequestBody(request);
+  }
+
+  private byte[] readRequestBody(HttpServletRequest request) {
+    try (InputStream inputStream = request.getInputStream()) {
+      return inputStream.readAllBytes();
+    } catch (IOException e) {
+      log.warn("Error during reading req. body: {}", e.getMessage(), e);
+      return new byte[0];
+    }
   }
 
   @Override
