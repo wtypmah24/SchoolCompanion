981dea746a14c5bd9f83c6e92b0b57df122b0e14
wtypmah24 <wtypmah24@gmail.com>
2025-07-12 13:56:18 +0200

    fix: cors

--- src/main/java/org/back/beobachtungapp/config/SecurityConfig.java
+++ src/main/java/org/back/beobachtungapp/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
             "https://kinder-compass-companion.onrender.com",
             "http://localhost:4173",
             "http://localhost:8080",
-            "https://kinder-compass-companion-314503497094.europe-west3.run.app"));
+            "https://kinder-compass-companion-314503497094.europe-west3.run.app",
+            "http://192.168.178.31:3000"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
