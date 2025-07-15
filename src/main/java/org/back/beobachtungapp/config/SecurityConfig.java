bdf7b38d05b3699bdcd86e816a88980dd506e504
wtypmah24 <wtypmah24@gmail.com>
2025-07-15 15:59:46 +0200

    fix: cors

--- src/main/java/org/back/beobachtungapp/config/SecurityConfig.java
+++ src/main/java/org/back/beobachtungapp/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
             "http://localhost:8080",
             "https://kinder-compass-companion-314503497094.europe-west3.run.app",
             "http://192.168.178.31:3000",
-            "http://wtypmah.duckdns.org:3000"));
+            "http://wtypmah.duckdns.org:3000",
+            "https://wtypmah.duckdns.org:3000"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
